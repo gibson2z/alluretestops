cff55ec6ea9e19c954f1ed91b1c1d93fd70f029c
Alexander Chechulin <bmw.f25@gmail.com>
2026-04-03 17:36:29 +0300

    Update TestWithSteps.java

--- src/test/java/io/qameta/allure/TestWithSteps.java
+++ src/test/java/io/qameta/allure/TestWithSteps.java
@@ -57,7 +57,7 @@ public class TestWithSteps {
     }
 
     @TM4J("AE-T4")
-    @DisplayName(name = "Update issue using two parameters")
+    @DisplayName("Update issue using two parameters")
     @Story("Update existing issue")
     @Microservice("Repository")
     @Tags({@Tag("api"), @Tag("regress4")})
