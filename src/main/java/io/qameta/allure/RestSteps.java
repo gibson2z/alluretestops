7bb707956a5b9453fe2219bfb917ee20054f4f40
Alexander Chechulin <bmw.f25@gmail.com>
2026-04-03 16:10:22 +0300

    Update RestSteps.java

--- src/main/java/io/qameta/allure/RestSteps.java
+++ src/main/java/io/qameta/allure/RestSteps.java
@@ -23,4 +23,26 @@ public class RestSteps {
         step(String.format("GET /repos/%s/%s/issues/%s", owner, repo, 10));
     }
 
+    // ADD THESE THREE MISSING METHODS:
+
+    @Step("Retrieve issue with title `{title}`")
+    public void shouldRetrieveIssueWithTitle(final String owner, final String repo, final String title) {
+        step(String.format("GET /repos/%s/%s/issues?title=%s", owner, repo, title));
+        // Add assertions here to verify the issue was retrieved correctly
+    }
+
+    @Step("Update issue title from `{oldTitle}` to `{newTitle}`")
+    public void updateIssueTitle(final String owner, final String repo, final String oldTitle, final String newTitle) {
+        step(String.format("GET /repos/%s/%s/issues?title=%s", owner, repo, oldTitle));
+        step(String.format("PATCH /repos/%s/%s/issues/%s", owner, repo, 10));
+        step(String.format("Title updated from '%s' to '%s'", oldTitle, newTitle));
+        // Add logic to find the issue ID and update it
+    }
+
+    @Step("List all issues in repository")
+    public void shouldListAllIssues(final String owner, final String repo) {
+        step(String.format("GET /repos/%s/%s/issues", owner, repo));
+        step("Verify that all issues are listed correctly");
+        // Add assertions to verify the list of issues
+    }
 }
