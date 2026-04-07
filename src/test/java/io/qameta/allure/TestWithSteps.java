38dd0867a370ed8f7b84393e08ff8e81b36626b5
Alexander Chechulin <bmw.f25@gmail.com>
2026-04-07 13:36:19 +0300

    Update TestWithSteps.java

--- src/test/java/io/qameta/allure/TestWithSteps.java
+++ src/test/java/io/qameta/allure/TestWithSteps.java
@@ -62,7 +62,7 @@ public class TestWithSteps {
     @Microservice("Repository")
     @Tags({@Tag("api"), @Tag("regress4")})
     @Description("Verify updating of an existing issue")
-    @ParameterizedTest(name = "{DisplayName}")
+    @ParameterizedTest(name = "{displayName}")
     @MethodSource("provideTitleUpdateData")
     public void shouldUpdateUserNote(@Param(value = "Old Title") String oldTitle, @Param(value = "New Title") String newTitle) {
         steps.createIssueWithTitle(OWNER, REPO, oldTitle);
