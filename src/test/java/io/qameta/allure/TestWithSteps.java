66dd4ab74bea922b63106e9de25acc6e71b8e105
Alexander Chechulin <bmw.f25@gmail.com>
2026-04-09 11:33:02 +0300

    Update TestWithSteps.java

--- src/test/java/io/qameta/allure/TestWithSteps.java
+++ src/test/java/io/qameta/allure/TestWithSteps.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.params.provider.Arguments;
